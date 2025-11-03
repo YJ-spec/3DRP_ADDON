5fc13cc78be362498d5fae8a10da3fda72af5143
zealot771220 <zealot771220@gmail.com>
2025-11-03 17:08:24 +0800

    test3

--- my-addon/run.py
+++ my-addon/run.py
@@ -139,7 +139,7 @@ def generate_mqtt_discovery_config(device_name, device_mac, sensor_type, sensor_
             "identifiers": f"{device_name}_{device_mac}",
             "name": f"{device_name}_{device_mac}",
             "model": device_name,
-            "manufacturer": device_name,
+            "manufacturer": device_name
             # "sw_version": ADDON_VERSION
         }
     }
@@ -172,7 +172,7 @@ def generate_mqtt_discovery_textconfig(device_name, device_mac, sensor_type, sen
             "identifiers": f"{device_name}_{device_mac}",
             "name": f"{device_name}_{device_mac}",
             "model": device_name,
-            "manufacturer": device_name,
+            "manufacturer": device_name
             # "sw_version": ADDON_VERSION
         }
     }
